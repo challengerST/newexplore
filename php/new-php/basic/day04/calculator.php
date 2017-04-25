0beb6a08d18af30c04fb3642269daba5dc5db7f8
challenger <18137279513@163.com>
2017-04-25 10:23:19 +0800

    repair selfcalculator.php

--- php/new-php/basic/day04/calculator.php
+++ php/new-php/basic/day04/calculator.php
@@ -1,83 +1,100 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Calculator</title>
-  <style>
-  tr{
-      display:inline-block;
-      border:1px solid #000;
-  }
-  </style>
-</head>
+<html>
+	<head>
+		<title>PHP实现简单计算器</title>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+	</head>
 <?php
-$num1=true;
-$num2=true;
-$numa=true;
-$numb=true;
-echo $_GET.'<br>';
-print $_GET.'<br>';
-print_r($_GET);
-if(isset($_GET['sub'])){
-    $sum=0;
-    if($num1 && $num2 && $numa && $numb){
-        switch($_GET['ysf']){
-case '+':
-$sum=$_GET['fnum'] + $_GET['bnum'];
-break;
-case '-':
-$sum=$_GET['fnum'] - $_GET['bnum'];
-break;
-case '*':
-$sum=$_GET['fnum'] * $_GET['bnum'];
-break;
-case '/':
-$sum=$_GET['fnum'] / $_GET['bnum'];
-break;
-case '%':
-$sum=$_GET['fnum'] + $_GET['bnum'];
-break;
-        }
-    }
-}
+	$num1=true;
+	$num2=true;
+	$numa=true;
+	$numb=true;
+	$message="";
+	//单路分支
+	if(isset($_GET["sub"])){
+		if($_GET["num1"]==""){
+			$num1=false;
+			$message.="第一个数不能为空";
+
+		}
+		if(!is_numeric($_GET["num1"])){
+			$numa=false;
+			$message.="第一个数不是数字";
+		}
+
+		if($_GET["num2"]==""){
+			$num2=false;
+			$message.="第二个数不能为空";
+
+		}
+		if(!is_numeric($_GET["num2"])){
+			$numb=false;
+			$message.="第二个数不是数字";
+		}
+		
+
+		if($num1 && $num2 && $numa && $numb) {
+			$sum=0;
+			//多路分支switch
+			switch($_GET["ysf"]){
+				case "+":
+					$sum=$_GET["num1"]+$_GET["num2"];
+					break;
+				case "-":
+					$sum=$_GET["num1"]-$_GET["num2"];
+					break;
+				case "x":
+					$sum=$_GET["num1"]*$_GET["num2"];
+					break;
+				case "/":
+					$sum=$_GET["num1"]/$_GET["num2"];
+					break;
+				case "%":
+					$sum=$_GET["num1"]%$_GET["num2"];
+					break;
+			}
+		}
+	}
 ?>
-<body>
-  <div>
-  <table>
-  <form action="calculator.php">
-  <tr>
-  <td>
-      <input type="text" name="fnum">
-  </td>
-  <td>
-  <select type="text" name="ysf">
-  <option value="+">+</option>
-  <option value="-">-</option>
-  <option value="*">*</option>
-  <option value="/">/</option>
-  <option value="%">%</option>
-  </select>
-  </td>
-  <td>
-  <input type="text" name="bnum">
-  </td>
-  <td>
-  <input type="submit" name="sub" value="计算">
-  </td>
-  <?php
-  if(isset($_GET['sub'])){
-echo '<tr><td colspan="5">';
-if($num1 && $num2 && $numa &&$numb){
-echo $_GET['fnum'] . $_GET['ysf'] . $_GET['bnum'] . '=' . $sum;
-};
-echo '</td></tr>';
-  }
-  ?>
-  </tr>
-  </form>  
-  </table>
-  </div>
-</body>
+
+	<body>
+		<table align="center" border="1" width="500">
+			<caption><h1>计算器</h1></caption>
+			<form action="jsq.php">
+			<tr>
+				<td>
+					<input type="text" size="5" name="num1" value="<?php echo $_GET["num1"] ?>" >
+				</td>
+				<td>
+					<select name="ysf">
+						<option value="+" <?php if($_GET["ysf"]=="+") echo "selected" ?>>+</option>
+						<option value="-" <?php if($_GET["ysf"]=="-") echo "selected" ?>>-</option>
+						<option value="x" <?php echo $_GET["ysf"]=="x"?"selected":"" ?>>x</option>
+						<option value="/" <?php echo $_GET["ysf"]=="/"?"selected":"" ?>>/</option>
+						<option value="%" <?php echo $_GET["ysf"]=="%"?"selected":"" ?>>%</option>
+					</select>
+				</td>
+				<td>
+					<input type="text" size="5" name="num2" value="<?php echo $_GET["num2"] ?>">
+				</td>
+				<td>
+					<input type="submit" name="sub" value="计算">
+				</td>
+			</tr>
+
+			<?php
+				if(isset($_GET["sub"])){
+
+					echo '<tr><td colspan="5">';
+					if($num1 && $num1 && $numa && $numb){
+						echo "结果：".$_GET["num1"]." ".$_GET["ysf"]." ".$_GET["num2"]." = ".$sum;
+					}else{
+						echo $message;
+					}	
+					echo '</td></tr>';
+				}
+			?>
+			</form>
+			</table>
+
+	</body>
 </html>
--- php/new-php/basic/day04/selfcalculator.php
+++ php/new-php/basic/day04/selfcalculator.php
@@ -5,19 +5,25 @@
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>self_calculator</title>
+  <style>
+    tr{
+      display:inline-block;
+      border:1px solid #000;
+  }
+  </style>
 </head>
 <?php
-print_r($_GET);
 $num1=true;
 $num2=true;
 $numa=true;
 $numb=true;
 $err="";
+if(isset($_GET['sub'])){
 if(empty($_GET['fnum'])){
 $num1=false;
 $err="数字不能为空";
 }
-if(is_numeric($_GET['fnum'])){
+if(!is_numeric($_GET['fnum'])){
 $numa=false;
 $err="请填写数字";
 }
@@ -25,24 +31,31 @@ if(empty($_GET['bnum'])){
 $num2=false;
 $err="数字不能为空";
 }
-if(is_numeric($_GET['bnum'])){
+if(!is_numeric($_GET['bnum'])){
 $numb=false;
 $err="请填写数字";
 }
-if(isset($_GET['sub'])){
+
 $sum=0;
+echo $num1;
+echo $num2;
 if($num1 && $num2 && $numa && $numb){
   switch($_GET['ysf']){
    case "+":
    $sum=$_GET['fnum']+$_GET['bnum'];
+   break;
    case "-":
    $sum=$_GET['fnum']-$_GET['bnum'];
+   break;
    case "*":
    $sum=$_GET['fnum']*$_GET['bnum'];
+   break;
    case "/":
    $sum=$_GET['fnum']/$_GET['bnum'];
+   break;
    case "%":
    $sum=$_GET['fnum']%$_GET['bnum'];
+   break;
   }
 }
 }
@@ -53,22 +66,24 @@ if($num1 && $num2 && $numa && $numb){
   <tr>
   <td><input type="text" name="fnum" value="<?php echo $_GET['fnum']?>" ></td>
   <td><select type="text" name="ysf">
-  <option value="+" <?php echo $_GET['ysf'] == '+' ?"selected" : "";?>>+</option>
-  <option value="-" <?php echo $_GET['ysf'] == '-'?"selected":"" ?>>-</option>
-  <option value="*" <?php echo $_GET['ysf'] == '*'?"selected":"" ?>>*</option>
-  <option value="/" <?php echo $_GET['ysf'] == '/'?"selected":"" ?>>/</option>
-  <option value="%" <?php echo $_GET['ysf'] == '%'?"selected":"" ?>>%</option>
+  <option value="+" <?php echo $_GET['ysf'] == "+" ?"selected" : "";?>>+</option>
+  <option value="-" <?php echo $_GET['ysf'] == "-"?"selected":"" ?>>-</option>
+  <option value="*" <?php echo $_GET['ysf'] == "*"?"selected":"" ?>>*</option>
+  <option value="/" <?php echo $_GET['ysf'] == "/"?"selected":"" ?>>/</option>
+  <option value="%" <?php echo $_GET['ysf'] == "%"?"selected":"" ?>>%</option>
   </select>
   </td>
   <td><input type="text" name="bnum" value="<?php echo $_GET['bnum']?>"></td>
-  <td><input type="submit" name="sub"></td>  
+  <td><input type="submit" name="sub"></td>
   </tr>
   <?php
-echo "<tr>";  
+  if(isset($_GET['sub'])){
+echo "<tr>";
   if($num1 && $num2 && $numa && $numb){
-echo "<td>".$_GET['fnum'].$_GET['ysf'].$_GET['bnum']."=".$num."</td>";
+echo "<td>".$_GET['fnum'].$_GET['ysf'].$_GET['bnum']."=".$sum."</td>";
   };
-echo "</tr>";  
+echo "</tr>";
+  }
   ?>
   </form>
   </table>
